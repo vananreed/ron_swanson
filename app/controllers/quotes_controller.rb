f3a7c0b8f92a8380f916cefac8ba33ad330fc27e
Reed Van Anrooy <reed.vananrooy@gmail.com>
2019-04-11 19:10:01 -0400

    Prevents duplicate quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,8 @@ class QuotesController < ApplicationController
 
   def create
     size = params[:size]
-    @quote = quote(size)
+    last_quote = params[:last_quote] ? Quote.find(params[:last_quote]) : Quote.first
+    @quote = quote(size, last_quote)
     redirect_to quotes_path(quote: @quote)
   end
 
@@ -11,23 +12,23 @@ class QuotesController < ApplicationController
     @review = Quotes::Review.new
   end
 
-  def small
-    Quote.where("length < 5")
+  def small(last_quote)
+    Quote.where.not(text: last_quote.text).where("length < 5")
   end
 
-  def medium
-    Quote.where("length > 4 and length < 13")
+  def medium(last_quote)
+    Quote.where.not(text: last_quote.text).where("length > 4 and length < 13")
   end
 
-  def large
-    Quote.where("length > 12")
+  def large(last_quote)
+    Quote.where.not(text: last_quote.text).where("length > 12")
   end
 
-  def any
-    Quote.all
+  def any(last_quote)
+    new_quote = Quote.all
   end
 
-  def quote(size)
-    public_send(size).sample
+  def quote(size, last_quote)
+    public_send(size, last_quote).sample
   end
 end
